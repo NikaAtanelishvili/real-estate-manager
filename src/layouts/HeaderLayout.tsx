d0fb0de25b726977dd4aea1f542fb09d5705f925
NikaAtanelishvili <atanelishvilinika@gmail.com>
2024-09-12 20:21:01 +0400

    feat: clicking on header redberry logo navigates to listings page

--- src/layouts/HeaderLayout.tsx
+++ src/layouts/HeaderLayout.tsx
@@ -1,12 +1,20 @@
 import redberryLogo from '@/assets/images/redberry-logo.png'
+import { useNavigate } from 'react-router-dom'
 
 const HeaderLayout: React.FC<{
   children: React.ReactNode
 }> = props => {
+  const navigate = useNavigate()
+
   return (
     <div>
       <header className="w-full border border-[#DBDBDB] px-40 py-9">
-        <img className="" src={redberryLogo} alt="Redberry Logo" />
+        <img
+          className="cursor-pointer"
+          onClick={() => navigate('/')}
+          src={redberryLogo}
+          alt="Redberry Logo"
+        />
       </header>
       <main>{props.children}</main>
     </div>
