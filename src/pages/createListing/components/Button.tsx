7eba6500af5e4e123df5f27485970bc65cde2928
NikaAtanelishvili <atanelishvilinika@gmail.com>
2024-09-13 16:58:08 +0400

    feat: add button component for create listing form

---
+++ src/pages/createListing/components/Button.tsx
@@ -0,0 +1,28 @@
+import { PlusSvg } from '@/assets'
+
+interface ButtonProps {
+  type: 'submit' | 'button'
+  backgroundColor: string
+  text: string
+  textColor: string
+  onClick?: () => void
+}
+
+const Button: React.FC<ButtonProps> = props => {
+  return (
+    <button
+      form="createListing"
+      onClick={props.onClick}
+      type={props.type}
+      style={{ backgroundColor: props.backgroundColor }}
+      className={`flex h-[50px] items-center justify-center gap-2 rounded-lg border border-[#F93B1D] px-4`}
+    >
+      <PlusSvg color={props.textColor} />
+      <p style={{ color: props.textColor }} className={`font-medium leading-5`}>
+        {props.text}
+      </p>
+    </button>
+  )
+}
+
+export default Button
